46103ab61c1fc58a091b28d28810c1db34ed63da
Chaithra KC <chaithra.kc22@gmail.com>
2026-08-27 21:17:34 -0400

    Tree map with comparator

---
+++ src/main/java/com/chay/corejava/collections/map/EX02_TreeMapCustomComparator.java
@@ -0,0 +1,24 @@
+package com.chay.corejava.collections.map;
+
+import java.util.TreeMap;
+import java.util.Comparator;
+
+class StrDescComparator implements Comparator<String> {
+    @Override
+    public int compare(String s1, String s2) {
+        return s2.compareTo(s1); // reverse order
+    }
+}
+
+public class EX02_TreeMapCustomComparator {
+    public static void main(String[] args) {
+        TreeMap<String, Integer> t = new TreeMap<>(new StrDescComparator());
+        t.put("XXX", 10);
+        t.put("AAA", 20);
+        t.put("ZZZ", 30);
+        t.put("LLL", 40);
+
+        System.out.println(t); // {ZZZ=30, XXX=10, LLL=40, AAA=20} - keys sorted in descending order
+
+    }
+}
